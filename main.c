2d63be7d48515e84c4dc83d9ba3513cc05d74363
mnmlyn <mingaoyang@sina.com>
2017-07-18 23:50:56 +0800

    Update main.c

--- main.c
+++ main.c
@@ -3,29 +3,34 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* nextGreaterElement(int* nums1, int nums1Size, int* nums2, int nums2Size, int* numsReturnSize) {
-    int i,j;
     int *numsReturn = (int *)malloc(sizeof(int)*nums1Size);
-    int flag;
-    *numsReturnSize = nums1Size;
+    //用栈来辅助，求num2中，每个数字后面更大的数
+    int *stack = (int *)malloc(sizeof(int)*nums2Size);
+    int *greater = (int *)malloc(sizeof(int)*nums2Size);
+    int *sb = stack,*sp = stack;
+    int i,j;
+    *sp = nums2[nums2Size-1];sp++;//入栈
+    greater[nums2Size-1] = -1;
+    for(i=nums2Size-2; i>=0; i--){
+        while(nums2[i]>*(sp-1) && sp!=sb){
+            sp--;
+        }
+        if(sp!=sb){
+            greater[i] = *(sp-1);
+        }
+        else{
+            greater[i] = -1;
+        }
+        *(sp++) = nums2[i]; 
+    }
+    //从nums1中寻找对应的数字
     for(i=0; i<nums1Size; i++){
-        flag = 0;
         for(j=0; j<nums2Size; j++){
-            if(!flag){
-                if(nums1[i] != nums2[j]){
-                    continue;
-                }
-                else{
-                    flag = 1;
-                }
+            if(nums1[i] == nums2[j]){
+                numsReturn[i] = greater[j];
             }
-            else{
-                if(nums2[j] > nums1[i]){
-                    numsReturn[i] = nums2[j];
-                    break;
-                }
-            }
-        }//for j
-        if(j == nums2Size)numsReturn[i] = -1;
-    }//for i
+        }
+    }
+    *numsReturnSize = nums1Size;
     return numsReturn;
 }
